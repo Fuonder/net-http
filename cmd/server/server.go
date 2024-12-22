874f14f0e7cbfa701657f2bd1322ebc739689cf8
Fuonder <ranygi23@mail.ru>
2024-12-22 21:01:59 +0300

    redirection example

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,27 +5,12 @@ import (
 	"net/http"
 )
 
-type Middleware func(http.Handler) http.Handler
-
-func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
-	for _, middleware := range middlewares {
-		h = middleware(h)
-	}
-	return h
-}
-
-func middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
-
-func rootHandle(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://yandex.ru/", http.StatusMovedPermanently)
 }
 
 func StartVMServer() error {
-	http.Handle("/", Conveyor(http.HandlerFunc(rootHandle)))
+	http.HandleFunc("/search/", redirect)
 	err := http.ListenAndServe("192.168.0.157:8080", nil)
 	if err != nil {
 		return fmt.Errorf("VM server start: %w", err)
