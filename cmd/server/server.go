68bd77ba8382c768662e0a218260722d31c3f469
Fuonder <ranygi23@mail.ru>
2024-12-22 21:03:12 +0300

    inline redirect

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -10,6 +10,7 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func StartVMServer() error {
+	http.Handle("/dummy", http.RedirectHandler("https://google.com", http.StatusMovedPermanently))
 	http.HandleFunc("/search/", redirect)
 	err := http.ListenAndServe("192.168.0.157:8080", nil)
 	if err != nil {
